471b1328f5e9d43cecb947070919c392b21a9826
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-06-05 17:54:43 +0200

    feat(libzappy_net): implement network interface initialization and cleanup functions

---
+++ libzappy_net/src/libzappy_net.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** Network interface implementation
+*/
+
+#include "libzappy_net.h"
+
+int zappy_net_init(void)
+{
+    /* Initialize network resources */
+    return 0;
+}
+
+void zappy_net_cleanup(void)
+{
+    /* Cleanup network resources */
+}
