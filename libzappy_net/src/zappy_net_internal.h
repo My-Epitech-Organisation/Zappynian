a2b6f13b972a94392392e19e4f0f4a45ed0d41cd
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-06-11 16:27:07 +0200

    feat(zappy_net): add ring buffer support to internal socket structure

--- libzappy_net/src/zappy_net_internal.h
+++ libzappy_net/src/zappy_net_internal.h
@@ -9,6 +9,7 @@
     #define ZAPPY_NET_INTERNAL_H_
 
     #include "../include/zappy_net.h"
+    #include "../include/zappy_net_ringbuf.h"
     #include <netinet/in.h>
 
 /**
@@ -19,6 +20,9 @@ struct zn_socket {
     int initialized;
     struct sockaddr_in addr;
     int type;
+    zn_ringbuf_t read_buffer;  /* Buffer for reading from socket */
+    zn_ringbuf_t write_buffer; /* Buffer for writing to socket */
+    int buffer_initialized;    /* Flag to track if buffers are initialized */
 };
 
 #endif /* !ZAPPY_NET_INTERNAL_H_ */
