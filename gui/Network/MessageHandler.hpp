43624e3dd8517d03b055224929860fc26cc14088
EvannBloutin <evann.bloutin@epitech.eu>
2025-06-12 16:00:35 +0200

    refactor(INetworkHandler, MessageHandler): improve code formatting for better readability

--- gui/Network/MessageHandler.hpp
+++ gui/Network/MessageHandler.hpp
@@ -6,16 +6,17 @@
 */
 
 #pragma once
-#include <unordered_map>
-#include <memory>
 #include "INetworkHandler.hpp"
+#include <memory>
+#include <unordered_map>
 
 class MessageHandler {
 public:
-    void registerHandler(const std::string& command, std::shared_ptr<INetworkHandler> handler);
-    void handleCommand(const std::string& cmd, const std::vector<std::string>& args);
+  void registerHandler(const std::string &command,
+                       std::shared_ptr<INetworkHandler> handler);
+  void handleCommand(const std::string &cmd,
+                     const std::vector<std::string> &args);
 
 private:
-    std::unordered_map<std::string, std::shared_ptr<INetworkHandler>> _handlers;
+  std::unordered_map<std::string, std::shared_ptr<INetworkHandler>> _handlers;
 };
-
