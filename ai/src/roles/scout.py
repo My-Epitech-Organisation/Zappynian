9933688d7bb1a33884f4dee554adb7d369016438
Rosh7887epitech <robin.schuffenecker@epitech.eu>
2025-06-12 15:35:08 +0200

    feat(ScoutRole): enhance decision-making logic in Scout role

--- ai/src/roles/scout.py
+++ ai/src/roles/scout.py
@@ -1,5 +1,24 @@
 from ai.src.roles.base_role import Role
+import random
 
 class Scout(Role):
     def __init__(self):
         super().__init__("Scout")
+
+    def decide(self, queue, world, vision):
+        nbr = random.randint(1, 3)
+
+        queue.push("Look")
+        if nbr == 1:
+            queue.push("Forward")
+            queue.push("Look")
+        elif nbr == 2:
+            queue.push("Right")
+            queue.push("Forward")
+            queue.push("Look")
+        elif nbr == 3:
+            queue.push("Left")
+            queue.push("Forward")
+            queue.push("Look")
+        else:
+            queue.push("Look")
