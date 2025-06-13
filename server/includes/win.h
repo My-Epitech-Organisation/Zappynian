9eec6cf6ba2049e17fe8cd91eb5789688d2b5886
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-13 11:49:21 +0200

    feat(win): create header file for win check functionality

---
+++ server/includes/win.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappynian
+** File description:
+** win
+*/
+
+#ifndef WIN_H
+    #define WIN_H
+
+    #include "player.h"
+
+bool win_check(const char *team_name, player_t **players, int player_count);
+
+#endif // WIN_H
