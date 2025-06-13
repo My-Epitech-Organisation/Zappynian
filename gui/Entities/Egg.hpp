b9874651b19c35f2912e0443487d2cd7929b1298
titouan.bouillot-bachelier <titouan.bouillot-bachelier@epitech.eu>
2025-06-13 15:56:52 +0200

    feat(entities): implement Egg class with constructor and type management

--- gui/Entities/Egg.hpp
+++ gui/Entities/Egg.hpp
@@ -5,13 +5,34 @@
 ** AEgg
 */
 
+#include "AEntity.hpp"
+#include "Event/EventReceiver.hpp"
+#include "irrlicht/irrlicht.h"
+#include <string>
+#include <vector>
+
 #pragma once
 
-class AEgg {
+class Egg : public AEntity {
 public:
-  AEgg();
-  ~AEgg();
+  Egg(int id, const irr::core::vector3df &pos,
+       const irr::core::vector3df &scale,
+       const std::vector<irr::io::path> &textures,
+       const irr::io::path &entityB3D)
+      : AEntity() type_("egg") {
+      setId(id);
+      setName("Egg");
+      setPosition(pos);
+      setScale(scale);
+      textures_(textures);
+      entityB3D_(entityB3D);
+  }
 
-protected:
+  ~Egg() override = default;
+
+  void setType(const std::string &type) { type_ = type; }
+  std::string getType() const { return type_; }
+
 private:
+  std::string type_;
 };
