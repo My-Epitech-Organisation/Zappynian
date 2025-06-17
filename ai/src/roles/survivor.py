8b561c22120979174b12b8117b4b65fdea91e2bd
Rosh7887epitech <robin.schuffenecker@epitech.eu>
2025-06-17 21:52:30 +0200

    fix(Survivor): enhance decision-making logic for food search and movement

--- ai/src/roles/survivor.py
+++ ai/src/roles/survivor.py
@@ -6,9 +6,15 @@ class Survivor(Role):
         super().__init__("Survivor")
 
     def decide(self, queue, world, vision):
+        print("[Survivor]")
         food_tile = vision.find_nearest("food")
         if food_tile != -1:
             path = route_to(food_tile)
             for cmd in path:
                 queue.send_and_wait(cmd)
             queue.send_and_wait("Take food")
+        else:
+            queue.send_and_wait("Right")
+            queue.send_and_wait("Forward")
+            queue.send_and_wait("Left")
+            queue.send_and_wait("Forward")
