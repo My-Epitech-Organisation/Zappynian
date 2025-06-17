61f8c2198209cbf601bfdbe9244bd4d4fa20c939
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-06-17 16:06:06 +0200

    refactor(server): simplify server connection initialization by removing redundant validation and setup functions

--- server/src/set_server.c
+++ server/src/set_server.c
@@ -7,38 +7,10 @@
 
 #include "../includes/server.h"
 
-static int validate_zappy_server(zn_socket_t zn_server)
-{
-    if (zn_server == NULL) {
-        return -1;
-    }
-    return 0;
-}
-
-static int setup_server_fd(server_connection_t *connection)
-{
-    connection->fd = zn_get_fd(connection->zn_server);
-    if (connection->fd == -1) {
-        return -1;
-    }
-    return 0;
-}
-
-int set_server_socket(server_connection_t *connection)
-{
-    connection->zn_server = zn_server_listen(connection->port);
-    if (validate_zappy_server(connection->zn_server) == -1) {
-        return -1;
-    }
-    if (setup_server_fd(connection) == -1) {
-        return -1;
-    }
-    return 0;
-}
-
 static int initialize_server_connection(server_connection_t *connection)
 {
-    if (set_server_socket(connection) == -1) {
+    connection->zn_server = zn_server_listen(connection->port);
+    if (connection->zn_server == NULL) {
         handle_error_connection("zn_server_listen", connection);
         return -1;
     }
