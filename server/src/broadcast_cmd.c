bf9557a76468d54c020271fb59668faad0ac1fdc
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-19 11:17:03 +0200

    fix(broadcast): remove unnecessary buffer allocation and free in broadcast_to_all_players

--- server/src/broadcast_cmd.c
+++ server/src/broadcast_cmd.c
@@ -65,10 +65,8 @@ void broadcast_to_all_players(player_t *sender, server_t *server,
     int direction = 0;
     char buffer[256];
 
-    if (server == NULL || server->players == NULL || message == NULL) {
-        free(buffer);
+    if (server == NULL || server->players == NULL || message == NULL)
         return;
-    }
     for (size_t i = 0; i < server->player_count; i++) {
         receiver = server->players[i];
         if (receiver == NULL || receiver->dead)
@@ -78,7 +76,6 @@ void broadcast_to_all_players(player_t *sender, server_t *server,
         snprintf(buffer, sizeof(buffer), "message %d, %s", direction, message);
         zn_send_message(server->connection->zn_server, buffer);
     }
-    free(buffer);
 }
 
 char *get_broadcast_message(player_t *player)
