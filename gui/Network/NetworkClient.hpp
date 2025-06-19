c39e0516bdb4727afe745b8169aaecd844657d78
titouan.bouillot-bachelier <titouan.bouillot-bachelier@epitech.eu>
2025-06-19 12:08:35 +0200

    feat(NetworkClient): implement createWorld method with player and team setup

--- gui/Network/NetworkClient.hpp
+++ gui/Network/NetworkClient.hpp
@@ -88,80 +88,80 @@ public:
 
   const std::string readLine(zn_socket_t sock);
 
-  // void createWorld() override {
-  //   createPlane(5, 5);
-  //   createLights();
-  //   createCamera();
-  //   createText();
-  //   addTeam("Red");
-  //   addTeam("Blue");
-  //   addTeam("Yellow");
-  //   addTeam("Purple");
-  //   addTeam("Orange");
-  //   addTeam("Green");
-  //   try {
-  //     createPlayer(1, 0, 0, Direction::NORTH, 0, "Red");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(1, 1);
-  //   try {
-  //     createPlayer(2, 1, 0, Direction::NORTH, 0, "Blue");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(2, 2);
-  //   try {
-  //     createPlayer(3, 2, 0, Direction::NORTH, 0, "Red");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(3, 3);
+  void createWorld() override {
+    createPlane(5, 5);
+    createLights();
+    createCamera();
+    createText();
+    addTeam("Red");
+    addTeam("Blue");
+    addTeam("Yellow");
+    addTeam("Purple");
+    addTeam("Orange");
+    addTeam("Green");
+    try {
+      createPlayer(1, 0, 0, Direction::NORTH, 0, "Red");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(1, 1);
+    try {
+      createPlayer(2, 1, 0, Direction::NORTH, 0, "Blue");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(2, 2);
+    try {
+      createPlayer(3, 2, 0, Direction::NORTH, 0, "Red");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(3, 3);
 
-  //   try {
-  //     createPlayer(4, 3, 0, Direction::NORTH, 0, "Yellow");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(4, 4);
-  //   try {
-  //     createPlayer(5, 4, 0, Direction::NORTH, 0, "Purple");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(5, 5);
-  //   try {
-  //     createPlayer(6, 0, 1, Direction::NORTH, 0, "Orange");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(6, 6);
-  //   try {
-  //     createPlayer(7, 0, 2, Direction::NORTH, 0, "Green");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(7, 7);
-  //   try {
-  //     createPlayer(8, 0, 3, Direction::NORTH, 0, "Green");
-  //   } catch (const std::exception &e) {
-  //   }
-  //   setPlayerLevel(8, 8);
+    try {
+      createPlayer(4, 3, 0, Direction::NORTH, 0, "Yellow");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(4, 4);
+    try {
+      createPlayer(5, 4, 0, Direction::NORTH, 0, "Purple");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(5, 5);
+    try {
+      createPlayer(6, 0, 1, Direction::NORTH, 0, "Orange");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(6, 6);
+    try {
+      createPlayer(7, 0, 2, Direction::NORTH, 0, "Green");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(7, 7);
+    try {
+      createPlayer(8, 0, 3, Direction::NORTH, 0, "Green");
+    } catch (const std::exception &e) {
+    }
+    setPlayerLevel(8, 8);
 
-  //   contentTiles(2, 4, 32, 32, 32, 32, 32, 32, 32);
-  //   contentTiles(4, 4, 32, 32, 32, 32, 32, 32, 32);
-  //   movePlayer(1, 2, 2, Direction::NORTH);
-  //   movePlayer(1, 2, 1, Direction::NORTH);
-  //   movePlayer(1, 2, 0, Direction::NORTH);
-  //   movePlayer(1, 2, 4, Direction::NORTH);
-  //   movePlayer(1, 1, 4, Direction::EAST);
-  //   movePlayer(1, 0, 4, Direction::EAST);
-  //   movePlayer(1, 4, 4, Direction::EAST);
-  //   movePlayer(1, 4, 0, Direction::SOUTH);
-  //   movePlayer(1, 4, 1, Direction::SOUTH);
-  //   movePlayer(1, 4, 2, Direction::SOUTH);
-  //   movePlayer(1, 0, 2, Direction::WEST);
-  //   movePlayer(2, 4, 2, Direction::NORTH);
-  //   PlayerInventory(1, 0, 2, 1, 1, 1, 14, 1, 1, 1);
-  //   startIncantation(4, 2, 2, {2});
-  //   // stopIncantation(4, 2, true);
-  //   // killPlayer(3);
-  //   broadcast(1, "Hello from player 1!");
-  //   broadcast(2, "Hello from player 2!");
-  // }
+    contentTiles(2, 4, 32, 32, 32, 32, 32, 32, 32);
+    contentTiles(4, 4, 32, 32, 32, 32, 32, 32, 32);
+    // movePlayer(1, 2, 2, Direction::NORTH);
+    // movePlayer(1, 2, 1, Direction::NORTH);
+    // movePlayer(1, 2, 0, Direction::NORTH);
+    // movePlayer(1, 2, 4, Direction::NORTH);
+    // movePlayer(1, 1, 4, Direction::EAST);
+    // movePlayer(1, 0, 4, Direction::EAST);
+    // movePlayer(1, 4, 4, Direction::EAST);
+    // movePlayer(1, 4, 0, Direction::SOUTH);
+    // movePlayer(1, 4, 1, Direction::SOUTH);
+    // movePlayer(1, 4, 2, Direction::SOUTH);
+    // movePlayer(1, 0, 2, Direction::WEST);
+    // movePlayer(2, 4, 2, Direction::NORTH);
+    // PlayerInventory(1, 0, 2, 1, 1, 1, 14, 1, 1, 1);
+    // startIncantation(4, 2, 2, {2});
+    // // stopIncantation(4, 2, true);
+    // // killPlayer(3);
+    // broadcast(1, "Hello from player 1!");
+    // broadcast(2, "Hello from player 2!");
+  }
 
   std::vector<std::shared_ptr<IEntity>> getEntities() {
     return WorldScene::getEntities();
