9aa53d54985de80d5f8de75dae6cba3a0fb20995
titouan.bouillot-bachelier <titouan.bouillot-bachelier@epitech.eu>
2025-06-19 10:24:36 +0200

    feat(main): update argument parsing to require host and port with flags

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -13,11 +13,30 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
-    return 1;
+  if (argc < 5) {
+    std::cerr << "Usage: " << argv[0] << "-p <port> -h <host>\n";
+    return -1;
   }
-  std::string host = argv[1];
+  if (std::string(argv[3]) != "-h" || std::string(argv[1]) != "-p") {
+    std::cerr << "Usage: " << argv[0] << "-p <port> -h <host>\n";
+    return -1;
+  }
+  if (std::string(argv[2]).empty() || std::string(argv[4]).empty()) {
+    std::cerr << "Host and port must not be empty.\n";
+    return -1;
+  }
+  if (std::stoi(argv[2]) <= 0 || std::stoi(argv[2]) > 65535) {
+    std::cerr << "Port must be a valid number between 1 and 65535.\n";
+    return -1;
+  }
+  if (argc > 5) {
+    std::cerr << "Too many arguments provided.\n";
+    return -1;
+  }
+  if (argc == 3 && std::string(argv[1]) == "-help") {
+    std::cout << "Usage: " << argv[0] << "-p port -h machine\n";
+  }
+  std::string host = argv[4];
   int port = std::stoi(argv[2]);
   Game game(host, port);
 
