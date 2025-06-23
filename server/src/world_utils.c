c42ef9e820f298076cc32610017423ad84230c18
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-23 15:27:14 +0200

    feat(world): implement resource management functions for tiles

---
+++ server/src/world_utils.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappynian
+** File description:
+** World utility functions
+*/
+
+#include "../include/world.h"
+
+int get_resource_type_from_name(const char *resource_name)
+{
+    char *resource_names[RESOURCE_COUNT] = {
+        "food", "linemate", "deraumere", "sibur", "mendiane", "phiras",
+        "thystame"
+    };
+
+    if (!resource_name)
+        return -1;
+    for (int i = 0; i < RESOURCE_COUNT; i++) {
+        if (strcmp(resource_name, resource_names[i]) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool take_resource_from_tile(tile_t *tile, int resource_type)
+{
+    if (!tile || resource_type < 0 || resource_type >= RESOURCE_COUNT)
+        return false;
+    if (tile->resources[resource_type] > 0) {
+        tile->resources[resource_type]--;
+        return true;
+    }
+    return false;
+}
