cb2f3c11ca0baf4f0b30800bd8f60b04cbff4650
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-23 15:26:47 +0200

    fix(commands): implement take command logic and resource handling

--- server/src/commands_actions.c
+++ server/src/commands_actions.c
@@ -5,8 +5,9 @@
 ** Action command implementations
 */
 
-#include <stdio.h>
 #include "../include/commands.h"
+#include "../include/server.h"
+#include "../include/world.h"
 
 void cmd_eject(player_t *player, server_t *server)
 {
@@ -16,8 +17,25 @@ void cmd_eject(player_t *player, server_t *server)
 
 void cmd_take(player_t *player, server_t *server)
 {
-    (void) server;
-    printf("Player %d executed take command\n", player->id);
+    int resource_type;
+    tile_t *current_tile;
+
+    if (player->dead || player->in_elevation)
+        return (void)zn_send_message(server->connection->zn_server, "ko");
+    resource_type = get_resource_type_from_name(get_command_argument(player));
+    if (resource_type == -1) {
+        zn_send_message(server->connection->zn_server, "ko");
+        return;
+    }
+    current_tile = get_tile(server->map, player->x, player->y);
+    if (take_resource_from_tile(current_tile, resource_type)) {
+        if (resource_type == 0)
+            player->food++;
+        else
+            player->resources[resource_type]++;
+        zn_send_message(server->connection->zn_server, "ok");
+    } else
+        zn_send_message(server->connection->zn_server, "ko");
 }
 
 void cmd_set(player_t *player, server_t *server)
