ad704eba7593ccdafdb12b90ba750df3f27c673e
titouan.bouillot-bachelier <titouan.bouillot-bachelier@epitech.eu>
2025-06-24 17:45:54 +0200

    refactor(WorldScene): remove debug output from killEgg function

--- gui/Core/WorldScene.cpp
+++ gui/Core/WorldScene.cpp
@@ -317,7 +317,6 @@ void WorldScene::killPlayer(int id) {
 void WorldScene::killEgg(int id) {
   for (auto it = entity_.begin(); it != entity_.end();) {
     if ((*it)->getId() == -7) {
-      std::cout << "Killing egg with ID: " << id << std::endl;
       auto egg = std::dynamic_pointer_cast<Egg>(*it);
       if (egg)
         egg->getNode()->remove();
