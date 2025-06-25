1f7a28d9ddd7ccca9d3503f403740c7e8700261b
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-25 18:09:59 +0200

    refactor(elevation): simplify incantation function signatures by removing unnecessary parameters

--- server/include/elevation.h
+++ server/include/elevation.h
@@ -21,20 +21,14 @@ typedef struct elevation_requirement_s {
 } elevation_requirement_t;
 
 void elevation_init_requirements(elevation_requirement_t *requirements);
-void start_incantation(tile_t *tile, int player_level,
-    const elevation_requirement_t *requirements);
-bool complete_incantation(tile_t *tile, int player_level,
-    const elevation_requirement_t *requirements, server_t *server);
+void start_incantation(tile_t *tile);
 void apply_elevation(tile_t *tile, int player_level,
-    const elevation_requirement_t *requirements, server_t *server);
+    const elevation_requirement_t *requirements);
 void cancel_incantation(tile_t *tile, int player_level);
-bool can_start_incantation(tile_t *tile, int player_level,
+bool can_start_incantation(tile_t *tile, player_t *player,
     const elevation_requirement_t *requirements);
 void check_and_send_elevation_status(server_t *server, player_t *player,
     tile_t *current_tile, const elevation_requirement_t *requirements);
-void complete_incantation_ritual(player_t *player, server_t *server);
 size_t count_players_with_level(tile_t *tile, player_t *player);
-int check_good_nb_players_and_resources(tile_t *tile,
-    elevation_requirement_t *req, player_t *player);
 
 #endif // ELEVATION_H
