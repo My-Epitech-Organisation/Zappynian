619520dd6c6d7e14c9d906309071432a3ddd3f25
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-25 18:24:23 +0200

    refactor(send_state_msg): remove redundant send_ko and send_ok functions to streamline player messaging

--- server/include/server.h
+++ server/include/server.h
@@ -114,8 +114,6 @@ void player_found(player_t *player, const char *line, client_t *client);
 
 const char **get_resource_names(void);
 
-void send_ko_to_all_players(server_t *server, tile_t *current_tile, size_t i);
-void send_ok_to_all_players(server_t *server, tile_t *current_tile, size_t i);
 void send_stat_to_all_players(server_t *server, tile_t *current_tile,
     size_t i, const char *stat_msg);
 
--- server/src/send_state_msg_to_players.c
+++ server/src/send_state_msg_to_players.c
@@ -7,30 +7,6 @@
 
 #include "../include/server.h"
 
-void send_ko_to_all_players(server_t *server, tile_t *current_tile, size_t i)
-{
-    client_t *fail_client;
-
-    if (current_tile->players[i] && current_tile->players[i]->in_elevation) {
-        fail_client = find_client_by_player(server, current_tile->players[i]);
-        if (fail_client) {
-            zn_send_message(fail_client->zn_sock, "ko");
-        }
-    }
-}
-
-void send_ok_to_all_players(server_t *server, tile_t *current_tile, size_t i)
-{
-    client_t *elevated_clt;
-
-    if (current_tile->players[i] && current_tile->players[i]->in_elevation) {
-        elevated_clt = find_client_by_player(server, current_tile->players[i]);
-        if (elevated_clt) {
-            zn_send_message(elevated_clt->zn_sock, "ok");
-        }
-    }
-}
-
 void send_stat_to_all_players(server_t *server, tile_t *current_tile,
     size_t i, const char *stat_msg)
 {
