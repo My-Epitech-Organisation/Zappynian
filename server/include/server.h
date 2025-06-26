e000b32baf7fadf676daed3960f35a117b443cd8
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-06-26 11:48:37 +0200

    fix(server): update send_bct function signature to remove map parameter

--- server/include/server.h
+++ server/include/server.h
@@ -145,7 +145,7 @@ int send_graphic_initial_state(client_t *client, server_args_t *args);
 void send_msz(zn_socket_t sock, size_t width, size_t height);
 void send_sgt(zn_socket_t sock, int frequency);
 void send_mct(zn_socket_t sock, map_t *map);
-void send_bct(zn_socket_t sock, tile_t *tile, map_t *map);
+void send_bct(zn_socket_t sock, tile_t *tile);
 void send_tna(zn_socket_t sock, team_t *teams, int team_count);
 void send_ewn(zn_socket_t sock, server_t *server);
 
