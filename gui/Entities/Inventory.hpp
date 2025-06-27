4a2cdd1ca1624d28ee8e9fda4ff68f38da3ea40f
EvannBloutin <evann.bloutin@epitech.eu>
2025-06-27 16:37:36 +0200

    feat(Inventory): enhance documentation for Inventory class methods

--- gui/Entities/Inventory.hpp
+++ gui/Entities/Inventory.hpp
@@ -11,13 +11,38 @@
 
 class Inventory {
 public:
+  /**
+   * @brief Default constructor for Inventory
+   */
   Inventory();
+  
+  /**
+   * @brief Destructor for Inventory
+   */
   ~Inventory();
 
+  /**
+   * @brief Adds items to the inventory
+   * 
+   * @param item Name of the item to add
+   * @param quantity Quantity of items to add
+   */
   void addItem(const std::string &item, size_t quantity);
 
+  /**
+   * @brief Removes items from the inventory
+   * 
+   * @param item Name of the item to remove
+   * @param quantity Quantity of items to remove
+   */
   void removeItem(const std::string &item, size_t quantity);
 
+  /**
+   * @brief Gets the quantity of a specific item
+   * 
+   * @param item Name of the item to check
+   * @return Quantity of the specified item
+   */
   size_t getItemQuantity(const std::string &item) const;
 
 protected:
