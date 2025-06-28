278dfd12e8d1d1744217538186425e0c281248fb
Rosh7887epitech <robin.schuffenecker@epitech.eu>
2025-06-28 09:45:09 +0200

    refactor(breeder): streamline decision logic and enhance broadcast handling

--- ai/src/roles/breeder.py
+++ ai/src/roles/breeder.py
@@ -4,30 +4,39 @@ from ai.src.utils.route_factory import *
 class Breeder(Role):
     def __init__(self):
         super().__init__("Breeder")
-        self.target_direction = None
+        self.target_path = None
 
     def decide(self, queue, world, vision):
-        # if self.target_direction is not None:
-        #     print(f"Breeder : Moving to target direction: {self.target_direction}")
-        #     path = goal_to(self.target_direction)
-        #     for cmd in path:
-        #         queue.send_and_wait(cmd)
-        #     self.target_direction = None
-        #     return
+        if self.target_path:
+            for cmd in self.target_path:
+                queue.send_and_wait(cmd)
+            queue.send_and_wait("Look")
+            self.target_path = None
+            return
         queue.send_and_wait("Fork")
 
     def on_broadcast(self, message, queue, world, vision):
-        return
-        # parts = message.split(",", 1)
-        # if len(parts) != 2:
-        #     return
-        # try:
-        #     tokens = parts[0].split()
-        #     if len(tokens) < 2 or not tokens[1].isdigit():
-        #         return
-        #     direction = int(tokens[1])
-        # except (IndexError, ValueError):
-        #     return
-        # content = parts[1].strip()
-        # if "incantation" in content.lower():
-        #     self.target_direction = direction
+        print(f"[BREEDERMESS] {message}")
+        parts = message.split(",", 1)
+        if len(parts) != 2:
+            return
+        if parts[1] == f"leader_{world.level}":
+            world.leader = True
+            return
+        elif parts[1] == "notleader":
+            world.leader = False
+            return
+        elif parts[1].startswith("incantation"):
+            try:
+                msg = parts[1].strip().split("_", 2)
+                if msg[1] != world.team_name or msg[2] != str(world.level):
+                    return
+                direction_str, content = parts
+                tokens = direction_str.strip().split()
+                if len(tokens) < 2:
+                    return
+                direction = int(tokens[1])
+                content = content.strip().lower()
+                self.target_path = goal_to(direction)
+            except Exception as e:
+                print(f"[WARN] Failed to parse broadcast: {e}")
