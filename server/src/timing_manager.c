ac1e7172d2ed650215571a017868e70fe44717fa
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-06-29 02:07:24 +0200

    refactor: remove redundant time utility functions from timing manager

--- server/src/timing_manager.c
+++ server/src/timing_manager.c
@@ -6,24 +6,8 @@
 */
 
 #include "../include/timing_manager.h"
-#include <stdlib.h>
+#include "../include/time_utils.h"
 #include <errno.h>
-
-uint64_t get_current_time_us(void)
-{
-    struct timeval tv;
-
-    if (gettimeofday(&tv, NULL) != 0)
-        return 0;
-    return (uint64_t)tv.tv_sec * 1000000 + (uint64_t)tv.tv_usec;
-}
-
-uint64_t calculate_tick_interval(int frequency)
-{
-    if (frequency <= 0)
-        return 0;
-    return 1000000 / frequency;
-}
 
 int timing_manager_init(timing_manager_t *tm, int frequency)
 {
