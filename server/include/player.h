a9f83603c05fad16a45f7ebd41e6c6d299d5672e
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-06-29 02:06:14 +0200

    feat: add has_pending_commands function declaration to player header

--- server/include/player.h
+++ server/include/player.h
@@ -43,5 +43,6 @@ player_t *create_player(int id, const char *team_name, int x, int y);
 void destroy_player(player_t *player);
 void move_player(player_t *player, int new_x, int new_y, map_t *map);
 bool player_decrement_food(player_t *player);
+bool has_pending_commands(player_t *player);
 
 #endif // PLAYER_H
